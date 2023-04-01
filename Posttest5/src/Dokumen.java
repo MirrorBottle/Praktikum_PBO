5da3f5618a87bfa05025062f8449136b15e53036
MirrorBottle <setiawanbayu66152@gmail.com>
2023-04-02 03:59:54 +0800

    feat: posttest5

---
+++ Posttest5/src/Dokumen.java
@@ -0,0 +1,46 @@
+public abstract class Dokumen {
+  private String NIK;
+  private String name;
+  private String address;
+
+  public abstract void description();
+  public Dokumen(String NIK, String name, String address) {
+    this.name = name;
+    this.NIK = NIK;
+    this.address = address;
+  }
+
+  public Dokumen() {}
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setNIK(String nIK) {
+    NIK = nIK;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getNIK() {
+    return NIK;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void description(boolean isApprove) {
+    if(isApprove) {
+      System.out.println("Apabila berkas yang diajukan sudah diterima, harap segera datang ke disdukcapil terdekat");
+    } else {
+      System.out.println("Apabila pengajuan berkas ditolak, harap melakukan pengajuan ulang setelah 24 Jam");
+    }
+  }
+}
